a0bf7d538c89ae5f39b9f6863faeb1e0a249e15d
NeoZet <smirnovmaksim10@mail.ru>
2018-09-19 00:45:17 +0300

    Add legend, remove some hardcode

--- num_meth/sin.py
+++ num_meth/sin.py
@@ -5,34 +5,46 @@ import decimal
 
 NUMBER_OF_FUNCTIONS = 10
 
+def partial_sum(func_sequence, args, number_of_elements):
+    partial_sums_list = []
+    for arg in args:        
+        partial_sums_list.append(func_sequence(arg, number_of_elements))
+    return partial_sums_list
+
+
 def taylor_sin(arg, number_of_elements):
     res = 0
     for i in range(number_of_elements+1):
         res += ((-1) ** i) * (arg ** (2 * i + 1)) / math.factorial(2 * i + 1)
     return res
 
-def partial_sum(sequence_func, args, number_of_elements):
-    seq_part = []
-    for arg in args:        
-        seq_part.append(sequence_func(arg, number_of_elements))
-    return seq_part
 
-def create_sin(x_args):
+def plot_sin(x_args):
     y = np.sin(x_args)
-    plt.plot(x_args, y, color='black', linewidth=2)
+    plt.plot(x_args, y, color='black', linewidth=2, label='$y=sin(x)$')
 
-def create_sin_approximation(args):
+
+def plot_partial_sums(func_sequence, args, number_of_members):
     plt.ylim(-2, 2)
-    for elem_num in range(NUMBER_OF_FUNCTIONS):
-        plt.plot(args, partial_sum(taylor_sin, args, elem_num), label='n={}'.format(elem_num))
+    for elem_num in range(number_of_members):
+        plt.plot(args, partial_sum(func_sequence, args, elem_num), label='$n={}$'.format(elem_num))
+
+
+def create_approximation(func, args, number_of_members, accuracy):
     
+        
+
 def main():
-    ax = plt.subplots()
     args = np.arange(-2 * np.pi, 2 * np.pi, 0.01)
-    create_sin_approximation(args)
-    create_sin(args)
+
+    plot_partial_sums(taylor_sin, args, NUMBER_OF_FUNCTIONS)    
+    plot_sin(args)
+
+    plt.legend(loc=3, ncol=6, mode="expand")
+    plt.title("Approximation of " + r"$y=sin(x)$")
+    plt.xlabel('X, [rad]')
+    plt.ylabel('Y, [ ]')
     plt.grid(True)
-    ax.legend(ncol=ncols, loc='best')
     
 if __name__ == "__main__":
     main()
