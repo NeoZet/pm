f15d093d415010ca670e756aa276d57cdc699b14
NeoZet <smirnovmaksim10@mail.ru>
2018-11-27 14:50:00 +0300

    Add database part for cassandra

--- database/lab_5/database.js
+++ database/lab_5/database.js
@@ -1,82 +1,97 @@
 var http = require('http')
-var sqlite3 = require('sqlite3').verbose();
-var db;
+var cassandra = require('cassandra-driver');
+var client;
+
+client = new cassandra.Client({
+	contactPoints: ['127.0.0.1'],
+	localDataCenter: 'datacenter1',
+});
 
 function createDb() {
-    db = new sqlite3.Database('persons.db', createTable);
+	client.connect()
+		.then(function() {
+			const query = "CREATE KEYSPACE IF NOT EXISTS lab5 WITH replication =" +
+				"{'class': 'SimpleStrategy', 'replication_factor': '1' }";
+
+			return client.execute(query);
+		})
+		.then(function() {
+			const query = "CREATE TABLE IF NOT EXISTS lab5.persons " +
+				"(ID int, FIRST_NAME TEXT, LAST_NAME TEXT, AGE int, PRIMARY KEY (ID))";
+			return client.execute(query);
+		})
+		.then(function() {
+			const query = "CREATE CUSTOM INDEX ON lab5.persons ( FIRST_NAME ) USING 'org.apache.cassandra.index.sasi.SASIIndex' WITH OPTIONS = {'analyzed' : 'true','analyzer_class' : 'org.apache.cassandra.index.sasi.analyzer.NonTokenizingAnalyzer','case_sensitive' : 'false', 'mode' : 'CONTAINS'}";
+			return client.execute(query);
+		})
+		.then(function() {
+			var query = "INSERT INTO lab5.persons (ID, FIRST_NAME, LAST_NAME, AGE) VALUES (?, ?, ?, ?)";
+
+			params = [1111, "Erofei", "Pavlov", 23];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1112, "Erofei", "Ivanov", 19];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1121, "Ivan", "Emin", 20];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1211, "Jhon", "Smirnov", 21];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [2111, "Semyon", "Pavlov", 20];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1311, "Andrey", "Novov", 19];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [4111, "Alex", "Emin", 18];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1113, "Genry", "Smirnov", 21];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1141, "Genry", "Pavlov", 20];
+			client.execute(query, params, {
+				prepare: true
+			});
+			params = [1411, "Anton", "No", 19];
+			client.execute(query, params, {
+				prepare: true
+			});
+		});
+	return client;
 }
 
-function openDb() {
-	db = new sqlite3.Database('./persons.db', sqlite3.OPEN_READWRITE, (err) => {
-  		if (err) {
-    		console.error(err.message);
-  		}
+function findByFirstName(clt, pattern) {
+	const query = "SELECT * FROM lab5.persons where FIRST_NAME LIKE ?";
+	return clt.execute(query, ['%' + pattern + '%'], {
+		prepare: true
 	});
 }
 
-function createTable() {
-    db.run("CREATE TABLE IF NOT EXISTS persons(ID INTEGER, FIRST_NAME TEXT, LAST_NAME TEXT, AGE INTEGER)", setRows);
-}
-
-function setRows() {
-    var stmt = db.prepare("INSERT INTO PERSONS VALUES (?, ?, ?, ?)");
-
-    stmt.run(1111, "Erofei", "Pavlov", 23);
-    stmt.run(1112, "Erofei", "Ivanov", 19);
-    stmt.run(1121, "Ivan", "Emin", 20);
-    stmt.run(1211, "Jhon", "Smirnov", 21);
-    stmt.run(2111, "Semyon", "Pavlov", 20);
-    stmt.run(1311, "Andrey", "Novov", 19);
-    stmt.run(4111, "Anton", "Emin", 18);
-    stmt.run(1113, "Genry", "Smirnov", 21);
-    stmt.run(1141, "Genry", "Pavlov", 20);
-    stmt.run(1411, "Anton", "No", 19);
-    stmt.run(4111, "Anton", "Emin", 18);    
-}
-
-var html_print = function (err, rows) {
-    if(err) {
-		console.log("ERROR");
-    }
-    http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type':'text/html'});
-	response.write('<!DOCTYPE html>\n' +
-		   '<html>\n'+
-		   '<head>\n' +
-		   '<meta charset=\'utf-8\'>\n' +
-		   '</head>\n'+
-		   '<body>\n');
-	response.write('<table border="1px" cellspacing="0px" width="30%" height="10%">\n');
-	response.write('<tr>\n'+
-		       '<td style="color: black">\nФамилия Имя</td>\n'+
-		       '<td id="str" align="center">\nВозрст</td>\n'+
-		       '<td id="numb1" align="center">\nID</td>\n'+
-		       '</tr>\n');
-	for(var i = 0; i < rows.length; i++) {
-	    response.write('<tr>\n'+
-			   '<td style="color: black">\n'+ rows[i].LAST_NAME + ' ' + rows[i].FIRST_NAME + '</td>\n'+
-			   '<td id="str" align="center">\n' + rows[i].AGE + '</td>\n'+
-			   '<td id="numb1" align="center">\n' + rows[i].ID + '</td>\n'+
-			   '</tr>\n');
-	}
-	response.end('</table>\n'+
-		     '</body>\n'+
-		     '</html>\n');
-    }).listen(3001);
-}
-
-function readAllRows() {
-	console.log(db);
-    db.all("SELECT * FROM PERSONS", html_print);
-}
-
 function closeDb() {
-    db.close();
+	client.shutdown();
 }
 
 function run() {
 	openDb();
-    return db;
-}	
+	return db;
+}
 
-module.exports.run = run;
+// var cl = createDb();
+// findByFirstName(cl, "Genry")
+// 	.then(function(res) {
+// 		console.log(res);
+// 	});
+
+module.exports.create = createDb;
+module.exports.findByFirstName = findByFirstName;
--- database/lab_5/server.js
+++ database/lab_5/server.js
@@ -4,29 +4,31 @@ var db = require('./database.js');
 
 console.log("Start server");
 
-http.createServer(function(req, res) {	
+http.createServer(function(req, res) {
 	request = req.url.split('&');
-	res.writeHead(200, {'Content-Type':'text/html'});
-	var database = db.run();		
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	var client = db.create();
+	// console.log(client);
 	var rules_callbacks = {
-		'/fname' : findInFirst,
-		'/lname' : findInLast,
-		'/age' : findInAge,
-		'/id' : findInID
-	};	
+		'/fname': db.findByFirstName
+		// '/lname': findInLast,
+		// '/age': findInAge,
+		// '/id': findInID
+	};
 	if (request[0].localeCompare('/') == 0) {
 		fs.readFile("./ajax.html", function(err, cont) {
-				res.end(cont);
+			res.end(cont);
 		});
-	}
-	else if (request[0].localeCompare('/fname') == 0 || 
-			 request[0].localeCompare('/lname') == 0 ||
-			 request[0].localeCompare('/age') == 0 ||
-			 request[0].localeCompare('/id') == 0) 
-	{
-		findInDB(database, request[1], rules_callbacks[request[0]])
+	} else if (request[0].localeCompare('/fname') == 0 ||
+		request[0].localeCompare('/lname') == 0 ||
+		request[0].localeCompare('/age') == 0 ||
+		request[0].localeCompare('/id') == 0) {
+		findInDB(client, request[1], rules_callbacks[request[0]])
 			.then(resolve => {
 				let http_str = html_format(resolve);
+				console.log(resolve.rows);
 				return http_str;
 			})
 			.then(http_str => {
@@ -37,88 +39,79 @@ http.createServer(function(req, res) {
 }).listen(3000);
 
 
-function findInDB(database, pattern, rules_callback) {
+function findInDB(client, pattern, rules_callback) {
 	return new Promise((resolve, reject) => {
-		database.all("SELECT * FROM PERSONS", function (err, rows) {
-			if(err) {
-				var error = new Error("ERROR");
-				error.code = -1;				
-				reject(error);
- 			}
- 			data = [];
-			for(var i = 0; i < rows.length; i++) {			
-				data.push(rows[i]);			
-			}		
-			result_list = rules_callback(data, pattern);
-			resolve(result_list);	
-		});	
+		db.findByFirstName(client, pattern)
+			.then(function(res) {
+				resolve(res);
+			});
 	});
 }
 
-var findInFirst = function (rows, pattern) {
+var findInFirst = function(rows, pattern) {
 	result_list = [];
-	for(var i = 0; i < rows.length; i++) {	
-	    if(rows[i].FIRST_NAME.indexOf(pattern) > -1) {
-			result_list.push(rows[i]);   	
-	    }	    
+	for (var i = 0; i < rows.length; i++) {
+		if (rows[i]['first_name'].indexOf(pattern) > -1) {
+			result_list.push(rows[i]);
+		}
 	}
 	return result_list;
 }
 
 
-var findInLast = function (rows, pattern) {
+var findInLast = function(rows, pattern) {
 	result_list = [];
-	for(var i = 0; i < rows.length; i++) {
-	    if(rows[i].LAST_NAME.indexOf(pattern) > -1) {
-			result_list.push(rows[i]);   	
-	    }
+	for (var i = 0; i < rows.length; i++) {
+		if (rows[i].LAST_NAME.indexOf(pattern) > -1) {
+			result_list.push(rows[i]);
+		}
 	}
 	return result_list;
 }
 
-var findInAge = function (rows, pattern) {
+var findInAge = function(rows, pattern) {
 	result_list = [];
-	for(var i = 0; i < rows.length; i++) {
-	    if(rows[i].AGE.toString().indexOf(pattern) > -1) {
-			result_list.push(rows[i]);   	
-	    }	    
+	for (var i = 0; i < rows.length; i++) {
+		if (rows[i].AGE.toString().indexOf(pattern) > -1) {
+			result_list.push(rows[i]);
+		}
 	}
 	return result_list;
 }
 
-var findInID = function (rows, pattern) {
+var findInID = function(rows, pattern) {
 	result_list = [];
-	for(var i = 0; i < rows.length; i++) {
-	    if(rows[i].ID.toString().indexOf(pattern) > -1) {
-			result_list.push(rows[i]);   	
-	    }	    
+	for (var i = 0; i < rows.length; i++) {
+		if (rows[i].ID.toString().indexOf(pattern) > -1) {
+			result_list.push(rows[i]);
+		}
 	}
 	return result_list;
 }
 
 
 function html_format(rows) {
-	var str ='<!DOCTYPE html>\n' +
-		   	'<html>\n'+
-		   	'<head>\n' +
-		   	'<meta charset=\'utf-8\'>\n' +
-		   	'</head>\n'+
-		   	'<body>\n' +
-			'<table border="1px" cellspacing="0px" width="30%" height="10%">\n' +
-			'<tr>\n'+
-		    '<td style="color: black">\nФамилия Имя</td>\n'+
-		    '<td id="str" align="center">\nВозрст</td>\n'+
-		    '<td id="numb1" align="center">\nID</td>\n'+
-		    '</tr>\n';
-	for(var i = 0; i < rows.length; i++) {
-	   str += '<tr>\n'+
-	   '<td style="color: black">\n'+ rows[i].LAST_NAME + ' ' + rows[i].FIRST_NAME + '</td>\n'+
-	   '<td id="str" align="center">\n' + rows[i].AGE + '</td>\n'+
-	   '<td id="numb1" align="center">\n' + rows[i].ID + '</td>\n'+
-	   '</tr>\n';
+	var str = '<!DOCTYPE html>\n' +
+		'<html>\n' +
+		'<head>\n' +
+		'<meta charset=\'utf-8\'>\n' +
+		'</head>\n' +
+		'<body>\n' +
+		'<table border="1px" cellspacing="0px" width="30%" height="10%">\n' +
+		'<tr>\n' +
+		'<td style="color: black">\nФамилия Имя</td>\n' +
+		'<td id="str" align="center">\nВозрст</td>\n' +
+		'<td id="numb1" align="center">\nID</td>\n' +
+		'</tr>\n';
+	for (var i = 0; i < rows.length; i++) {
+		str += '<tr>\n' +
+			'<td style="color: black">\n' + rows[i].LAST_NAME + ' ' + rows[i].FIRST_NAME + '</td>\n' +
+			'<td id="str" align="center">\n' + rows[i].AGE + '</td>\n' +
+			'<td id="numb1" align="center">\n' + rows[i].ID + '</td>\n' +
+			'</tr>\n';
 	}
-	str += '</table>\n'+
-		    '</body>\n'+
-		    '</html>\n';
+	str += '</table>\n' +
+		'</body>\n' +
+		'</html>\n';
 	return str;
 }
