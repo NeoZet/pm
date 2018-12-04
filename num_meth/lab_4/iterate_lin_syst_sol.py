97dfa5921fb12ebdf297b08da72cc479598920e1
NeoZet <smirnovmaksim10@mail.ru>
2018-12-04 11:28:09 +0300

    Python fixes

--- num_meth/lab_4/iterate_lin_syst_sol.py
+++ num_meth/lab_4/iterate_lin_syst_sol.py
@@ -51,15 +51,11 @@ def jacobi_method(system, extens, eps):
 #def gauss_zeidel_meth(system, extens, eps):
 import numpy as np
 
-def gauss_seidel(a, b, errorfactor = 0.01):
-    a = np.array(a)
-    b = np.array(b)
-    l = np.zeros((len(a), len(a[0])))
-    for i in range(len(a)):
-        for j in range(i+1):
-            l[i][j] = float(a[i][j]) # Get the lower matrix of a
+def gauss_seidel(system, extens, eps):
+    a = np.array(system)
+    b = np.array(extens)
 
-    x0 = [0.0] * len(a[0])
+    x0 = [1.0] * len(a[0])
     xprev = np.array(x0)
     prevnorm = None
     while True:
@@ -70,14 +66,14 @@ def gauss_seidel(a, b, errorfactor = 0.01):
             prevnorm = rnorm
         elif prevnorm < rnorm: # It's going far away
             return xprev
-        ebnorm = errorfactor * np.linalg.norm(b)
+        ebnorm = eps * np.linalg.norm(b)
         if ebnorm > rnorm:
             return xprev 
         for i in range(len(a)): # Forward substitution
             cum = 0.0
             for j in range(i):
-                cum += float(l[i][j] * z[j])
-            z[i] = (r[i] - cum)/float(l[i][i])
+                cum += (a[i][j] * z[j])
+            z[i] = (r[i] - cum)/(a[i][i])
         xprev += z    
     
 def main():
