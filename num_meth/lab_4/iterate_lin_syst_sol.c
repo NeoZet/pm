4a2de154d8169041ea965f6640b3286c4ca1d012
maximsmi <maxim.smirnov@intel.com>
2018-12-04 22:52:22 +0300

    Add reading data from file

--- num_meth/lab_4/iterate_lin_syst_sol.c
+++ num_meth/lab_4/iterate_lin_syst_sol.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define SYSTEM_ORDER 3
+#define DEFAULT_ORDER 3
 
 int jacobi(double **matrix, double *extension, int order, double eps, double *solve);
 int gauss_seidel(double **matrix, double *extension, int order, double eps, double *solve);
 
-static int readEquations(FILE *fd, double **matrix, double *extension, int order);
+static int readEquations(char *filename, double ***matrix, double **extension, int *order);
 static int resid(double **matrix, double *extension, double *solve, int order, double *resid);
 static int resid_norm(double *resid, int order, double *norm);
 static double accuracy(double *solv_curr, double *solv_prev, int order);
 
-int main()
+int main(int argc, char *argv[])	
 {
-	int order = SYSTEM_ORDER;
-	int i;		
-	double **matrix = (double**)malloc(sizeof(double*) * order);
-	for (i = 0; i < order; ++i) {
-		matrix[i] = (double*)malloc(sizeof(double) * order);
-	}
-	double *extension = (double*)malloc(sizeof(double) * order);
+	int i;
+	int order;
+	double **matrix;
+	double *extension;
+	if (argc != 2) {
+		argv[1] = NULL;
+	}	
 	double *solve = (double*)malloc(sizeof(double) * order);
 	double *residial = (double*)malloc(sizeof(double) * order);
 	double residial_norm = 0;
+	int ret = readEquations(argv[1], &matrix, &extension, &order);
 
-	int ret = readEquations(NULL, matrix, extension, order);
 	if (ret != 0) {
 		printf("Matrix read error: %d\n", ret);
 		return ret;
@@ -168,30 +168,39 @@ static double accuracy(double *solv_curr, double *solv_prev, int order)
 }
 
 	
-static int readEquations(FILE *fd, double **matrix, double *extension, int order)
-{
-	double matrix_tmp[SYSTEM_ORDER][SYSTEM_ORDER] = {{5,-3,1},
-							 {1,-10,3},
-							 {-2,1,4}};/* {{5.0, 1.0, 0.0, -1.0}, */
-							/*  {3.0, -3.0, 1.0, 4.0}, */
-							/*  {3.0, 0.0, -2.0, 1.0}, */
-							/*  {1.0, -4.0, 0.0, 1.0}}; */
-	double extension_tmp[SYSTEM_ORDER] = /* {-9.0,-7.0,-16.0,0.0}; */{1,2,-8};
-	if (matrix == NULL || extension == NULL) {		
+static int readEquations(char *filename, double ***matrix, double **extension, int *order)
+{       
+	FILE *fd;
+	int order_tmp;
+	if (order == NULL) {
 		return -1;
 	}
-	if (fd == NULL) {
-		//TODO: read by stdio
-		int i, j;
-		for (i = 0; i < order; ++i) {
-			for (j = 0; j < order; ++j) {
-				matrix[i][j] = matrix_tmp[i][j];
-			}
-			extension[i] = extension_tmp[i];
-		}
-		return 0;
+	
+	if (filename == NULL) {
+		fd = stdin;
 	}
-	//TODO: read from fs
+	else {
+		fd = fopen(filename, "r");
+	}
+	
+	fscanf(fd, "%d", &order_tmp);
+	*order = order_tmp;
+	printf("%d\n", order_tmp);
+	int i, j;
+	(*matrix) = (double**)malloc(sizeof(double*) * order_tmp);
+	for (i = 0; i < order_tmp; ++i) {
+		(*matrix)[i] = (double*)malloc(sizeof(double) * order_tmp);
+	}	
+	(*extension) = (double*)malloc(sizeof(double) * order_tmp);
+	double elem;
+	for (i = 0; i < order_tmp; ++i) {
+		for (j = 0; j < order_tmp; ++j) {
+			fscanf(fd, "%lf,", &(*matrix)[i][j]);
+		}
+	}
+	for (i = 0; i < order_tmp; ++i) {
+		fscanf(fd, "%lf,", &(*extension)[i]);
+	}
 	return 0;
 }
 
