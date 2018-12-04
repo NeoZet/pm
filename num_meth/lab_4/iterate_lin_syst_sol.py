846ab66267cc9a1e6b093f9ba667a15e4c80213a
NeoZet <smirnovmaksim10@mail.ru>
2018-12-04 09:19:18 +0300

    Python part complete

--- num_meth/lab_4/iterate_lin_syst_sol.py
+++ num_meth/lab_4/iterate_lin_syst_sol.py
@@ -47,7 +47,38 @@ def jacobi_method(system, extens, eps):
         x_k = add_matr(mulmatr(C, x_k), b)
         norm = max(map(abs, sub_matr(trans_matr(x_k_prev), trans_matr(x_k))[0]))
     return x_k
-    
+
+#def gauss_zeidel_meth(system, extens, eps):
+import numpy as np
+
+def gauss_seidel(a, b, errorfactor = 0.01):
+    a = np.array(a)
+    b = np.array(b)
+    l = np.zeros((len(a), len(a[0])))
+    for i in range(len(a)):
+        for j in range(i+1):
+            l[i][j] = float(a[i][j]) # Get the lower matrix of a
+
+    x0 = [0.0] * len(a[0])
+    xprev = np.array(x0)
+    prevnorm = None
+    while True:
+        z = np.inner(a, xprev)
+        r = b - z
+        rnorm = np.linalg.norm(r)
+        if prevnorm is None:
+            prevnorm = rnorm
+        elif prevnorm < rnorm: # It's going far away
+            return xprev
+        ebnorm = errorfactor * np.linalg.norm(b)
+        if ebnorm > rnorm:
+            return xprev 
+        for i in range(len(a)): # Forward substitution
+            cum = 0.0
+            for j in range(i):
+                cum += float(l[i][j] * z[j])
+            z[i] = (r[i] - cum)/float(l[i][i])
+        xprev += z    
     
 def main():
     init_cond_filename = default_init_cond_filename
@@ -55,6 +86,7 @@ def main():
         init_cond_filename = sys.argv[1]
     A, b = read_system(init_cond_filename)    
     print(jacobi_method(A, b, 0.0001))
+    print(gauss_seidel(A, b, 0.0001))
     
 if __name__ == '__main__':
     main()
