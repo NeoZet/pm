99fe6b3262d57c27ea2ad46b74c8331d2525c92b
NeoZet <smirnovmaksim10@mail.ru>
2018-12-04 06:44:41 +0300

    Complete Jacobi method .py

--- num_meth/lab_4/iterate_lin_syst_sol.py
+++ num_meth/lab_4/iterate_lin_syst_sol.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from functools import reduce
 
 default_init_cond_filename = 'system.txt'
@@ -12,9 +13,17 @@ trans_matr = lambda x: [*zip(*x)]
 unit_matr = lambda ord: [[1 if i == j else 0 for i in range(ord)] for j in range(ord)]
 
 def mulmatr(A, B):
-    Bt = trans_matr(B)
-    return [[sum(elem_a*elem_b for elem_a, elem_b in zip(row_a, col_b)) 
-             for col_b in Bt] for row_a in A]
+    rows_A = len(A)
+    cols_A = len(A[0])
+    rows_B = len(B)
+    cols_B = len(B[0])
+    
+    C = [[0 for row in range(cols_B)] for col in range(rows_A)]
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
 
 def read_system(filename):
     with open(filename, 'r') as data_file:
@@ -26,33 +35,27 @@ def read_system(filename):
     
 
 def jacobi_method(system, extens, eps):
-    D = [system[i][i] for i in range(len(system))]
-    invers_D = [1/D[i][i] for i in range(len(D))]
+    D = [[system[i][j] if i == j else 0 for i in range(len(system))] for j in range(len(system))]
+    invers_D = [[1/D[i][i] if i == j else 0 for i in range(len(D))] for j in range(len(D))]
     E = unit_matr(len(system))
     C = sub_matr(E, mulmatr(invers_D, system))
-    b = mulmatr(invers_D, extens)
+    b = mulmatr(invers_D, trans_matr([extens]))
     x_k = [[1] for i in range(len(system))]
-    it = 100
-    while it:#norm > eps:
+    norm = 1
+    while norm > eps:        
+        x_k_prev = x_k
         x_k = add_matr(mulmatr(C, x_k), b)
-        it -= 1
-    printf(x_k)
+        norm = max(map(abs, sub_matr(trans_matr(x_k_prev), trans_matr(x_k))[0]))
+    return x_k
     
     
-
-
 def main():
     init_cond_filename = default_init_cond_filename
     if len(sys.argv) == 2:
         init_cond_filename = sys.argv[1]
-    A, b = read_system(init_cond_filename)
-    a = [[1,2,3], [2,3,4], [3,4,5]]
-    b = [[4,5,6], [5,6,7], [6,7,8]]
-    c = [1,2,3]
-    d = [4,5,6]
-    print([a[i][i] for i in range(len(a))])
-    #print([1/D[i][i] for i in range(len(D))])
-#    jacobi_method(A, b, 0.001)
+    A, b = read_system(init_cond_filename)    
+    print(jacobi_method(A, b, 0.0001))
     
 if __name__ == '__main__':
     main()
+
