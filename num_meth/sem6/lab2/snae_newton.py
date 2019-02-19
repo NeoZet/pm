261ac864a12dcb97e1304a4f458c8964fef7d928
maximsmi <maxim.smirnov@intel.com>
2019-02-19 21:09:01 +0300

    Add format output for solution

--- num_meth/sem6/lab2/snae_newton.py
+++ num_meth/sem6/lab2/snae_newton.py
@@ -1,5 +1,5 @@
 import numpy as np
-import copy 
+import copy
 
 def gauss(A, b):
     def _prepare_main_elem():
@@ -28,14 +28,11 @@ def gauss(A, b):
         solve[i] = (b[i] - sum_known_elems) / A[i][i]
     return solve;
 
-def resid(A, b, solve):
-    F = []
-    for i in range (len(A)):
-        res = 0
-        for j in range (len(solve)):
-            res += A[i][j] * solve[j]
-        F.append(res - b[i])
-    return F
+
+delta_1 = lambda system, xk: max([abs(func) for func in system(xk)])
+delta_2 = lambda xk, xk_next: max([abs(xi_next - xi) for xi, xi_next in zip(xk, xk_next)])
+get_exponent = lambda number: int(str(number).split('e')[1])
+format_float = lambda number: '{:.10f}'.format(number)
 
 def jacobian(f, x):
     h = 1.0e-4
@@ -50,34 +47,64 @@ def jacobian(f, x):
         jacobian[:,i] = (f_x - f0)/h
     return jacobian, f0
 
-def newton(f, x, tol=1.0e-9):
+def newton(f, x, eps_1, eps_2):
     iterMax = 500
+    d1 = []
+    d2 = []
+        
     for i in range(iterMax):
         Jac, f0 = jacobian(f, x)
-        if np.sqrt(np.dot(f0, f0) / len(x)) < tol:
-            return x, i                 
         dx = gauss(Jac, f0)
         if dx == 'e':
             print("Решений Нет!")
-            exit(1)
-        x = x - dx
-    print ("Слишком много итераций!")
+            return None
+        
+        x_prev = copy.deepcopy(x)
+        x -= dx
+        
+        d1.append(delta_1(f, x))
+        d2.append(delta_2(x_prev, x))        
+        
+        if d1[i] < eps_1 and d2[i] < eps_2:
+            return {'solution': x, 'iteration': i, 'delta_1': d1, 'delta_2': d2}
+        
+    print("Слишком много итераций!")
+    return None
+
+NUMBER_OF_EQUATIONS = 2
 
 def f(x):
-    f = np.zeros([2])
+    f = np.zeros([NUMBER_OF_EQUATIONS])
 
     """
     https://old.math.tsu.ru/EEResources/cm/text/3_3.htm
- """  
+   
     f[0] = 2 * x[1] - np.cos(x[0]+1)
     f[1] = x[0] + np.sin(x[1]) + 0.4
-#    """
+    """   
 
-    # f[0] = power(x[0], 2) - x[1] + 1
-    # f[1] = x[0] - cos(pi/2 * x[1])
+    # f[0] = 2*x[0] - np.sin((x[0]-x[1])/2)
+    # f[1] = 2*x[1] - np.cos((x[0]+x[1])/2)
+    
+    f[0] = np.power(x[0], 2) - x[1] + 1
+    f[1] = x[0] - np.cos(np.pi/2 * x[1])
     return f
 
-x0 = np.zeros([2])
-x, iter = newton(f, x0)
-print ('Solution:\n', x)
-print ('Newton iteration = ', iter)
+EPS_1 = 1.0e-9
+EPS_2 = 1.0e-9
+
+def main():
+    x0 = np.zeros([NUMBER_OF_EQUATIONS])
+    res = newton(f, x0, EPS_1, EPS_2)
+    print('''
+Iter   |      delta_1           |     delta_2
+---------------------------------------------------''')
+    for iter, d1, d2 in zip(range(res['iteration']), res['delta_1'], res['delta_2']):
+        print('''
+  {0}    |      {1}      |     {2} '''.format(iter, format_float(d1), format_float(d2)))
+
+    print ('\nSolution:\nX1 = {0}\nX2 = {1}\n'.format(format_float(res['solution'][0]),
+                                                    format_float(res['solution'][1])))
+    print ('Newton iteration = ', res['iteration'])
+if __name__ == '__main__':
+    main()
