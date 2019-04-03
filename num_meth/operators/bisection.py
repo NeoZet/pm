289076814b6716113c504ff75822adbdda0ec078
maximsmi <maxim.smirnov@intel.com>
2019-04-04 01:26:12 +0300

    Add bisectoin and newton methods

--- num_meth/operators/bisection.py
+++ num_meth/operators/bisection.py
@@ -4,40 +4,63 @@ import numpy as np
 EPS = 0.001
 F = 1
 
-left_bound = -30.0
-right_bound = 30.0
+LEFT_BOUND = -10.0
+RIGHT_BOUND = 10.0
 
 def func(x, F):
     return 6 - np.exp(-2 * x) + 2 * x - F
 
-def derivative(function, x, F, h=0.0001):
-    return (function(x + h, F) - function(x - h, F)) / (2 * h)
+def bisection_step(equation, x, F, left_bound, right_bound):
+    return ()
 
-
-# TODO change name
-def newton_step(equation, x, F):
-    return (x - equation(x, F) / derivative(equation, x, F))
-
-def newton(equation, x0, F, eps):
-    x = x0
-    x_next = newton_step(equation, x0, F)
-    while np.abs(x_next - x) > eps:
-        x = x_next
-        x_next = newton_step(equation, x_next, F)
-    return x_next
+def bisection(equation, left_bound, right_bound, F, eps):
+    mid = (left_bound + right_bound) / 2
+    while np.abs(left_bound - right_bound) and np.abs(equation(mid, F)) > eps:
+        f_left = equation(left_bound, F)
+        f_mid = equation(mid, F)
+        f_right = equation(right_bound, F)
+        
+        if f_left * f_mid > 0:
+            left_bound = mid
+        elif f_left * f_mid < 0:
+            right_bound = mid
+        mid = (left_bound + right_bound) / 2    
+    return mid
 
 
 def main():
     start_values = np.array([0.1, 0.001, 1])
 
-    x_list = np.arange(left_bound, right_bound, 0.01)
-    y_list = np.array([func(x, 0) for x in x_list])
-    print(func(-4, 0))
-    plt.plot(x_list, y_list, linewidth=2)
-    plt.title("Newton's method")
+    eq_x_list = np.arange(-40, 40, 0.01)
+    eq_y_list = np.array([func(x, 0) for x in eq_x_list])
+
+    F_x_list = np.arange(-40, 40, 0.01)
+    F_y_list = np.array([F for x in F_x_list])
+    
+    fig,ax = plt.subplots()
+    ax.grid(True)
+    ax.tick_params(axis='x', which='major', direction='inout',
+                bottom=True, top=False, left=True, right=False, 
+                color='b', labelcolor='black',
+                labelbottom=True, labeltop=False, labelleft=True, labelright=False)
+
+    plt.title("Bisection method")
     plt.ylim(-25, 30)
     plt.xlim(-20, 20)
-    plt.grid(True)
+
+    bisection_solution = bisection(func, LEFT_BOUND, RIGHT_BOUND, F, EPS)
+    
+    plt.plot(eq_x_list, eq_y_list, linewidth=2, label="y = 6 - e^(-2x) + 2x")
+    plt.plot(F_x_list, F_y_list, linewidth=2, color='red', label="F = 1")
+    plt.plot(bisection_solution,
+             F,
+             marker='o',
+             markersize=4,
+             color='black',
+             label='solution'
+    )
+    plt.annotate("[{0:.5f}, {1}]".format(bisection_solution, F), (bisection_solution, F))
+    plt.legend(loc='best')
     plt.show()
     
 if __name__ == '__main__':
--- num_meth/operators/newton.py
+++ num_meth/operators/newton.py
@@ -4,9 +4,6 @@ import numpy as np
 EPS = 0.001
 F = 1
 
-left_bound = -30.0
-right_bound = 30.0
-
 def func(x, F):
     return 6 - np.exp(-2 * x) + 2 * x - F
 
@@ -29,15 +26,38 @@ def newton(equation, x0, F, eps):
 
 def main():
     start_values = np.array([0.1, 0.001, 1])
+    x0 = 0.1
+    
+    eq_x_list = np.arange(-40, 40, 0.01)
+    eq_y_list = np.array([func(x, 0) for x in eq_x_list])
 
-    x_list = np.arange(left_bound, right_bound, 0.01)
-    y_list = np.array([func(x, 0) for x in x_list])
-    print(func(-4, 0))
-    plt.plot(x_list, y_list, linewidth=2)
+    F_x_list = np.arange(-40, 40, 0.01)
+    F_y_list = np.array([F for x in F_x_list])
+    
+    fig,ax = plt.subplots()
+    ax.grid(True)
+    ax.tick_params(axis='x', which='major', direction='inout',
+                bottom=True, top=False, left=True, right=False, 
+                color='b', labelcolor='black',
+                labelbottom=True, labeltop=False, labelleft=True, labelright=False)
+
     plt.title("Newton's method")
     plt.ylim(-25, 30)
     plt.xlim(-20, 20)
-    plt.grid(True)
+
+    newton_solution = newton(func, x0, F, EPS)
+    
+    plt.plot(eq_x_list, eq_y_list, linewidth=2, label="y = 6 - e^(-2x) + 2x")
+    plt.plot(F_x_list, F_y_list, linewidth=2, color='red', label="F = 1")
+    plt.plot(newton_solution,
+             F,
+             marker='o',
+             markersize=4,
+             color='black',
+             label='solution'
+    )
+    plt.annotate("[{0:.5f}, {1}]".format(newton_solution, F), (newton_solution, F))
+    plt.legend(loc='best')
     plt.show()
     
 if __name__ == '__main__':
