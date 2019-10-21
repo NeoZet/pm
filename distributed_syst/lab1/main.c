7d1170cadb4e6c239ec2bc913a2809ee4455f6b7
NeoZet <smirnovmaksim10@mail.ru>
2019-10-21 09:12:56 +0300

    [distributed_syst_lab1] Implement both generators and vectors operations

--- distributed_syst/lab1/main.c
+++ distributed_syst/lab1/main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
 #define MAX_FILE_NAME 64
-
+#define MAX_VECTOR_DIMENTION 128
 
 typedef struct {
-	int dimentions;
-	double *vector;
+	int dimention;
+	double components[MAX_VECTOR_DIMENTION];
 } vector_t;
 
 
@@ -21,15 +22,14 @@ typedef struct {
 } arguments_t;
 
 
-int vectors_sum(vector_t first_vector, vector_t second_vector, vector_t *sum);
-int vector_module(vector_t vector);
-int generate_vector(vector_t *vector);
+int generate_vector(int dimention, vector_t *vector);
+int generate_vectors(int vectors_number, int dimention, vector_t *vectors);
+int vectors_sum(vector_t *vectors, int vectors_number, vector_t *sum);
+int vector_module(vector_t vector, double *module);
 
-int generate_files(int quantity, char *prefix, char generated_files_names[][MAX_FILE_NAME]);
+int generate_files_names(int quantity, char *prefix, char generated_files[][MAX_FILE_NAME]);
 int parse_args(int argc, char *argv[], arguments_t *parsed_args);
 void print_usage(int argc, char *argv[]);
-
-
 
 
 int main(int argc, char *argv[])
@@ -45,38 +45,106 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	printf("%d\n%s\n%s\n%d\n%d\n", args.processes_number, args.input_prefix, args.output_prefix, args.vectors_number, args.vectors_dimention);
+	//	printf("%d\n%s\n%s\n%d\n%d\n", args.processes_number, args.input_prefix, args.output_prefix, args.vectors_number, args.vectors_dimention);
 
-	char input_files_names[args.processes_number][MAX_FILE_NAME];
+ 	char input_files_names[args.processes_number][MAX_FILE_NAME];
+	char output_files_names[args.processes_number][MAX_FILE_NAME];
 	
-	if ((ret = generate_files(args.processes_number, args.input_prefix, input_files_names)) != 0) {
+	if ((ret = generate_files_names(args.processes_number, args.input_prefix, input_files_names)) != 0) {
+		return ret;
+	}
+
+	if ((ret = generate_files_names(args.processes_number, args.output_prefix, output_files_names)) != 0) {
 		return ret;
 	}
 }
 
 
-int vectors_sum(vector_t first_vector, vector_t second_vector, vector_t *sum)
+int generate_files_names(int quantity, char *prefix, char generated_files[][MAX_FILE_NAME])
 {
+	char tmp_str[MAX_FILE_NAME];
+	if (prefix == NULL) {
+		printf("[generate_files_names] Invalid argument: prefix: NULL\n");
+		return -1;
+	}
 
+	for (int i = 0; i < quantity; ++i) {
+		snprintf(generated_files[i], MAX_FILE_NAME, "%s%d", prefix, i);
+	}
+	return 0;
+}
+
+int generate_vector(int dimention, vector_t *vector)
+{
+	if (vector == NULL) {
+		printf("[generate_vector] Invalid argument: NULL\n");
+		return -1;				
+	}
+
+	vector->dimention = dimention;
+	for (int i = 0; i < dimention; i++) {
+		vector->components[i] = rand() % 23;
+	}
+
+	return 0;
 }
 
 
-int vector_module(vector_t vector)
+int generate_vectors(int vectors_number, int dimention, vector_t *vectors)
 {
+	vector_t tmp_vec;
+	if (vectors == NULL) {
+		printf("[generate_vectors] Invalid argument: NULL\n");
+		return -1;				
+	}
 
+	for (int i = 0; i < vectors_number; i++) {
+		if (generate_vector(dimention, &tmp_vec) < 0) {
+			printf("[generate_vectors] Unable to generate vectors\n");
+			return -2;
+		}
+		vectors[i] = tmp_vec;
+	}
+
+	return 0;
 }
 
 
-int generate_vector(vector_t *vector)
+int vectors_sum(vector_t *vectors, int vectors_number, vector_t *sum)
 {
+	if (vectors == NULL || sum == NULL) {
+		printf("[vectors_sum] Invalid argument: NULL\n");
+		return -1;				
+	}
 
+	memset(sum->components, 0, sizeof(MAX_VECTOR_DIMENTION));
+	
+	sum->dimention = vectors[0].dimention;
+	for (int i = 0; i < vectors_number; ++i) {
+		for (int j = 0; j < vectors[i].dimention; j++) {
+			sum->components[j] += vectors[i].components[j];
+		}
+	}
+	return 0;
 }
 
 
-int generate_files(int quantity, char *prefix, char generated_files_names[][MAX_FILE_NAME])
+int vector_module(vector_t vector, double *module)
 {
+	if (module == NULL) {
+		printf("[vector_module] Invalid argument: NULL\n");
+		return -1;				
+	}
 
+	double sum = 0;
+	
+	for(int i = 0; i < vector.dimention; i++) {
+		sum += pow(vector.components[i], 2);
+	}
+	*module = sqrt(sum);
+	return 0;
 }
+
 
 
 // TODO: re-work parser with getopt_long()
@@ -89,7 +157,7 @@ int parse_args(int argc, char *argv[], arguments_t *parsed_args)
 	}
 
 	if (parsed_args == NULL) {
-		printf("[parse_args] Invalid argument: parsed_args: NULL");
+		printf("[parse_args] Invalid argument: parsed_args: NULL\n");
 		return -2;
 	}
 	
